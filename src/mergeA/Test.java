e3d943115ec74495818e849496196dd4fc832ccb
Hemendra Rajput <flp.hemendra123@gmail.com>
2025-01-19 18:41:34 +0530

    testb

--- src/mergeA/Test.java
+++ src/mergeA/Test.java
@@ -2,7 +2,7 @@ package mergeA;
 
 public class Test {
 	public static void main(String[] args) {
-      System.out.println("WorkplaceA");
+      System.out.println("WorkplaceB");
       System.out.println("Common");
 	}
 }
